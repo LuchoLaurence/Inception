2a70e9bd19fb53357cb56df3e77098dd63c33ef5
Luc <skallyx19@gmail.com>
2024-08-28 12:13:51 +0200

    Finish

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -8,12 +8,6 @@ done
 
 # Configurer wp-config.php avec les variables d'environnement
 
-# cp /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
-# sed -i "s/database_name_here/$DB_NAME/" /var/www/html/wp-config.php
-# sed -i "s/username_here/$DB_USER/" /var/www/html/wp-config.php
-# sed -i "s/password_here/$DB_PASSWORD/" /var/www/html/wp-config.php
-# sed -i "s/localhost/$DB_NAME/" /var/www/html/wp-config.php
-
 if [ ! -f "$WP_PATH/wp-config.php" ]; then
     wp core download --path=$WP_PATH --allow-root
 
@@ -23,10 +17,16 @@ if [ ! -f "$WP_PATH/wp-config.php" ]; then
 
     cd /var/www/html
 
+# Create wp-config.php
     wp config create --path=$WP_PATH --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASSWORD --dbhost=$DB_HOST --dbprefix="br_" --allow-root
+
+# Install WordPress
     wp core install --path=$WP_PATH --url=$WP_URL --title="$WP_TITLE" --admin_user=$WP_USERADMIN --admin_password=$WP_PASSWORDADMIN --admin_email=$WP_EMAILADMIN --allow-root
+
+# Create dummy user
     wp user create --path=$WP_PATH $WP_USERDUMMY $WP_EMAILDUMMY --role=editor --user_pass=$WP_PASSWORDDUMMY --allow-root
 
+# Give the right for all the new files
     chown -R www-data:www-data /var/www/html
     find /var/www/html -type d -exec chmod 755 {} \;
     find /var/www/html -type f -exec chmod 644 {} \;
